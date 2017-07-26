262a4f9b9872d4398d6f715a3df60ad891a7b28e
qrbnmhdyv <mehdizade948@gmail.com>
2017-07-26 17:40:24 +0400

    template
    1 section responsivliyi ok!

--- 25.072017/js.js
+++ 25.072017/js.js
@@ -1,17 +1,62 @@
-$("li a").click(function(event){
-  event.preventDefault()
-  $("nav").css("background","#1E7A46")
-  var elem=$("#"+$(this).attr("name"))
-$(window).scrollTop(elem.offset().top)
 
-console.log(elem.offset().top)
-  
-})
+$(window).ready(function() {
+$("#a").click(function(){
+    $("body").animate({
+      scrollTop:0
+      }, 1000); 
+    $("#ulnav2").hide()
+});
+
+$("#about1,#about2,#btn").click(function(){
+    $("body").animate({
+      scrollTop:700
+      }, 1000); 
+    $("#ulnav2").hide()
+});
+
+$("#services1,#services2,#btn2").click(function(){
+    $("body").animate({
+      scrollTop:1470
+      }, 1000); 
+    $("#ulnav2").hide()
+});
+
+$("#gallery1,#gallery2").click(function(){
+    $("body").animate({
+      scrollTop:2700
+      }, 1000); 
+    $("#ulnav2").hide()
+});
+
+
+$("test1").click(function(){
+    $("body").animate({
+      scrollTop:3000
+      }, 1000); 
+    $("#ulnav2").hide()
+});
+});
+
 $(window).on("scroll",function(){
   var count=$(this).scrollTop();
+  console.log(count/250)
   $("#nav").css({
     background:"rgba(30, 122, 70,"+count/250+" )"
   })
  
+  
+})
+$(window).on("scroll",function(){
+  var count=$(this).scrollTop();
+  console.log(count/250)
+  console.log($(this).scrollTop())
+  $("#ulnav2").css({
+    background:"rgba(30, 122, 70,"+count/250+" )"  
+  }) 
+})
+$("#ulnav2").hide()
+$(".btns").click(function(){
+  event.preventDefault()
+  $("#ulnav2").toggle(500);
   
 })
