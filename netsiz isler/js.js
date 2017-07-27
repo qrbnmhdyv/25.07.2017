9f838ff615e5691d59e0b719f68dcb3a0b8e368a
qrbnmhdyv <mehdizade948@gmail.com>
2017-07-27 15:20:31 +0400

    Add files via upload
    commit eledim dincel

---
+++ netsiz isler/js.js
@@ -0,0 +1,16 @@
+
+
+// var count=1;
+// var img=[];
+// var time=1000;
+
+
+// img=["./img/slider1.jpg","./img/slider2.jpg"]
+
+
+$("#nav2").hide()
+$(".icn").click(function(){
+  event.preventDefault()
+  $("#nav2").toggle(500);
+  
+})
